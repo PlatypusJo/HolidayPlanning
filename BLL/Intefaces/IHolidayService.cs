d657387966de30025f631b3e8376f6a3fefd88be
Nikita Reshetnikov <quggoif@mail.ru>
2025-02-25 11:00:52 +0300

    Add HolidayService

---
+++ BLL/Intefaces/IHolidayService.cs
@@ -0,0 +1,16 @@
+﻿using BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Intefaces
+{
+    /// <summary>
+    /// Интерфейс для сервиса Меропирятий
+    /// </summary>
+    public interface IHolidayService : ICrud<HolidayDto>
+    {
+    }
+}
---
+++ BLL/Services/HolidayService.cs
@@ -0,0 +1,88 @@
+﻿using BLL.Abstract;
+using BLL.DTOs;
+using BLL.Intefaces;
+using DAL.Entities;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class HolidayService : BaseService, IHolidayService
+    {
+        #region Конструкторы
+
+        /// <summary>
+        /// Контрукор на основе UnitOfWork
+        /// </summary>
+        /// <param name="unitOfWork">Экземпляр UnitOfWork</param>
+        public HolidayService(IUnitOfWork unitOfWork) : base(unitOfWork) { }
+        #endregion
+
+        #region Методы
+        public async Task<bool> Create(HolidayDto itemDto)
+        {
+            var holiday = new Holiday
+            {
+                Id = itemDto.Id,
+                Title = itemDto.Title,
+                StartTime = itemDto.StartTime,
+                EndTime = itemDto.EndTime,
+                Budget = itemDto.Budget
+            };
+
+            await _unitOfWork.Holiday.Create(holiday);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            if (!await _unitOfWork.Holiday.Exists(id))
+                return false;
+
+            await _unitOfWork.Holiday.Delete(id);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> Exists(int id) => await _unitOfWork.Holiday.Exists(id);
+
+        public async Task<List<HolidayDto>> GetAll()
+        {
+            var items = await _unitOfWork.Holiday.GetAll();
+
+            var result = items
+                .Select(item => new HolidayDto(item))
+                .ToList();
+
+            return result;
+        }
+
+        public async Task<HolidayDto> GetById(int id)
+        {
+            var item = await _unitOfWork.Holiday.GetItem(id);
+
+            return item == null ? null : new HolidayDto(item);
+        }
+
+        public async Task<bool> Update(HolidayDto itemDto)
+        {
+            if (!await _unitOfWork.Holiday.Exists(itemDto.Id))
+                return false;
+
+            Holiday item = await _unitOfWork.Holiday.GetItem(itemDto.Id);
+
+            item.Id = itemDto.Id;
+            item.Title = itemDto.Title;
+            item.StartTime = itemDto.StartTime;
+            item.EndTime = itemDto.EndTime;
+            item.Budget = itemDto.Budget;
+
+            await _unitOfWork.Holiday.Update(item);
+            return await SaveAsync();
+        }
+        #endregion
+    }
+}
