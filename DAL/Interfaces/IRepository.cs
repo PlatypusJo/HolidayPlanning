7f537570df5f5cfa86b53b5b47eb672583ce2d74
Nikita Reshetnikov <quggoif@mail.ru>
2025-02-25 10:42:27 +0300

    Add HolidayRepository, realize unitOfWork pattern

---
+++ DAL/Interfaces/IRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    /// <summary>
+    /// Интерфейс репозитория класса T
+    /// </summary>
+    /// <typeparam name="T">Тип класса</typeparam>
+    public interface IRepository<T> where T : class
+    {
+        /// <summary>
+        /// Возвращает список всех сущностей
+        /// </summary>
+        /// <returns>Список сущностей</returns>
+        Task<List<T>> GetAll();
+
+        /// <summary>
+        /// Ищет сущность по заданному ID
+        /// </summary>
+        /// <param name="id">ID сущности</param>
+        /// <returns> Найденная сущность (null, если сущность не найдена)</returns>
+        Task<T> GetItem(int id);
+
+        /// <summary>
+        /// Добавляет сущность в коллекцию
+        /// </summary>
+        /// <param name="item">Сущность</param>
+        Task Create(T item);
+
+        /// <summary>
+        /// Обновляет сущность в коллекции
+        /// </summary>
+        /// <param name="item">Сущность</param>
+        Task Update(T item);
+
+        /// <summary>
+        /// Удаляет из коллекции сущность по заданному ID
+        /// </summary>
+        /// <param name="id">ID сущности</param>
+        /// <returns>true, если удаление успешно, иначе false</returns>
+        Task<bool> Delete(int id);
+
+        /// <summary>
+        /// Проверяет наличие в коллекции сущности по заданному ID
+        /// </summary>
+        /// <param name="id">ID сущности</param>
+        /// <returns>true, если сущность существует, иначе false</returns>
+        Task<bool> Exists(int id);
+    }
+}
---
+++ DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,54 @@
+﻿using DAL.Entities;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        #region Поля
+        /// <summary>
+        /// Контекст базы данных
+        /// </summary>
+        private readonly HolidayDbContext _db;
+
+        /// <summary>
+        /// Репозиторий мероприятия
+        /// </summary>
+        private HolidayRepository _holidayRepository;
+        #endregion
+
+        #region Свойства
+        public IRepository<Holiday> Holiday
+        {
+            get
+            {
+                _holidayRepository ??= new HolidayRepository(_db);
+                return _holidayRepository;
+            }
+        }
+        #endregion
+
+        #region Конструкторы
+        /// <summary>
+        /// Конструктор, принимающий контекст базы данных
+        /// </summary>
+        /// <param name="db">Контекст базы данных</param>
+        public UnitOfWork(HolidayDbContext db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        #region Методы
+        public async Task<int> Save()
+        {
+            return await _db.SaveChangesAsync();
+        }
+        #endregion
+    }
+}
