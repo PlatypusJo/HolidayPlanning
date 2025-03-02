83e13f5e6fa62760df1f25dfe8a99852fbf8be39
Nikita Reshetnikov <quggoif@mail.ru>
2025-03-02 14:04:02 +0300

    Add required Interfaces. Update IUnitOfWork Interface

---
+++ DAL/Interfaces/IFrozenCollectionRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    /// <summary>
+    /// Интерфейс репозитория для справочников, содержимое которых не будет изменятся в процессе работы приложения
+    /// </summary>
+    /// <typeparam name="T">Тип справочника</typeparam>
+    public interface IFrozenCollectionRepository<T> where T : class
+    {
+        /// <summary>
+        /// Возвращает список всех сущностей
+        /// </summary>
+        /// <returns>Список сущностей</returns>
+        Task<List<T>> GetAll();
+    }
+}
