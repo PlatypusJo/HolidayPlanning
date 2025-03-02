b88faa2f03b23e418dd3059209e285abbdf16e4d
Nikita Reshetnikov <quggoif@mail.ru>
2025-03-02 16:12:14 +0300

    refs #39 Add interfaces for services for Contractor, ContractorCategory and ContractorStatus

---
+++ BLL/Intefaces/IContractorService.cs
@@ -0,0 +1,22 @@
+﻿using BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Intefaces
+{
+    /// <summary>
+    /// Интерфейс для сервиса Подрядчиков
+    /// </summary>
+    public interface IContractorService : ICrud<ContractorDto>
+    {
+        /// <summary>
+        /// Возвращает всех подрядчиков выбранного мероприятия в виде dto
+        /// </summary>
+        /// <param name="holidayId">ID Мероприятия</param>
+        /// <returns>Список dto сущностей</returns>
+        Task<List<ContractorDto>> GetAllByHolidayId(int holidayId);
+    }
+}
