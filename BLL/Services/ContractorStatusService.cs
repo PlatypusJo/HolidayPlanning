d8d3ff391fbcf91bef1b01b9f4321f32e3c4132f
Nikita Reshetnikov <quggoif@mail.ru>
2025-03-02 16:13:02 +0300

    refs #39 Add Services for Contractor, ContractorCategory and ContractorStatus. Change some comments

---
+++ BLL/Services/ContractorStatusService.cs
@@ -0,0 +1,43 @@
+﻿using BLL.Abstract;
+using BLL.DTOs;
+using BLL.Intefaces;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    /// <summary>
+    /// Сервис Статуса подрядчика
+    /// </summary>
+    public class ContractorStatusService : BaseService, IContractorStatusService
+    {
+        #region Конструкторы
+
+        /// <summary>
+        /// Контрукор на основе UnitOfWork
+        /// </summary>
+        /// <param name="unitOfWork">Экземпляр UnitOfWork</param>
+        public ContractorStatusService(IUnitOfWork unitOfWork) : base(unitOfWork) { }
+
+        #endregion
+
+        #region Методы
+
+        public async Task<List<ContractorStatusDto>> GetAll()
+        {
+            var items = await _unitOfWork.ContractorStatus.GetAll();
+
+            var result = items
+                .Select(item => new ContractorStatusDto(item))
+                .ToList();
+
+            return result;
+        }
+
+        #endregion
+    }
+}
